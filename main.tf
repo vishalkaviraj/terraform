791379949c5571c0034954789ef686bab7ad92e3
vishalkaviraj <165932340+vishalkaviraj@users.noreply.github.com>
2024-05-05 01:01:10 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,8 +24,8 @@ resource "snowflake_database" "demo_db" {
   comment = "Database for Snowflake Terraform demo"
 }
 
-resource "snowflake_database" "simple" {
-  name                        = "testing"
-  comment                     = "test comment"
-  data_retention_time_in_days = 3
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
 }
